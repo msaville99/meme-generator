474170cf3bb6733476d6b29c927365560ed5b5e6
Michael Saville <msaville09@gmail.com>
2022-10-14 11:36:56 -0400

    initial commit

---
+++ QuoteEngine/__init__.py
@@ -0,0 +1,1 @@
+from .quoteengine import QuoteEngine
---
+++ QuoteEngine/quoteengine.py
@@ -0,0 +1,17 @@
+class QuoteModel():
+
+    def __init__(self, body:str, author:str):
+        """Create quote object which encapsulates body and author
+        
+        Arguments:
+            body {str} -- body of text.
+            author {str} -- author.
+        Returns:
+            quote {obj} -- quote object.
+        """
+        self.body = body
+        self.author = author
+
+    def __str__(self):
+        return f'"{self.body}” - {self.author}'
+    
